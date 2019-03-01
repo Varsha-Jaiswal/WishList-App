c421408090479e2258645701e9a1b52d38a24a34
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2019-03-01 17:37:26 +0530

    Added actions

--- src/models/WishList.js
+++ src/models/WishList.js
@@ -2,6 +2,12 @@
 import { types } from 'mobx-state-tree';
 import { WishListItem } from './WishListItem';
 
-export const WishList = types.model({
+export const WishList = types
+  .model({
     items: types.optional(types.array(WishListItem), [])
-  });
+  })
+  .actions(self => ({
+    add(item) {
+      self.items.push(item)
+    }
+  }))
--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -7,6 +7,8 @@ it("can create a instance", () => {
     price: 20
   })
   expect(item.price).toBe(20)
+  item.changeName("AB")
+  expect(item.name).toBe("AB")
 })
 
 it("can create a wislist", () => {
@@ -20,4 +22,17 @@ it("can create a wislist", () => {
   })
   expect(list.items.length).toBe(1)
   expect(list.items[0].price).toBe(23)
+})
+
+it("can add new data" , () => {
+  const newData = WishList.create()
+  newData.add(
+    WishListItem.create({
+      name: "New item",
+      price: 40
+    })
+  )
+  expect(newData.items.length).toBe(1)
+  newData.items[0].changeName("AB")
+  expect(newData.items[0].name).toBe("AB")
 })

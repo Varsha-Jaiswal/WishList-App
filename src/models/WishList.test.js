35f9f6e896a26dfa1a2098d200791c953fe3bf05
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2019-03-06 10:20:17 +0530

    minor change

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,6 +1,7 @@
 import { WishListItem } from "./WishListItem"
 import { WishList } from "./WishList";
 import { getSnapshot, onSnapshot } from "mobx-state-tree";
+import { reaction } from "mobx";
 
 it("can create a instance", () => {
   const item = WishListItem.create({
@@ -39,4 +40,27 @@ it("can add new data" , () => {
   newData.items[0].changeName("AB")
   expect(newData.items[0].name).toBe("AB")
   expect(getSnapshot(newData)).toMatchSnapshot()
+})
+
+it('can calculate total price', () => {
+  const list = WishList.create({
+    items: [
+      {
+        name: "item 1",
+        price: 10,
+        image: ""
+      },
+      {
+        name: "item 2",
+        price: 23.4,
+        image: ""
+      }
+    ]
+  })
+  expect(list.totalPrice).toBe(33.4)
+  let changed = 0
+  reaction(() => list.totalPrice,() => changed++)
+  expect(changed).toBe(0)
+  list.items[0].changePrice(20)
+  expect(changed).toBe(1)
 })

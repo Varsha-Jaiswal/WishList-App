d345c245b929d37208ba729684dc702e3c44895c
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2019-03-06 12:28:03 +0530

    Added editing feature

--- src/components/WishListItemView.js
+++ src/components/WishListItemView.js
@@ -1,9 +1,58 @@
-import React from "react"
+import React, { Component } from "react"
 
-export const WishListItemView = ({ item }) => (
-    <li className="item">
-        { item.image && <img src = {item.image} alt=""/>}
-        <h3>{item.name}</h3>
-        <span>{item.price}</span>
-    </li>
-)
+import WishListItemEdit from "./WishListItemEdit"
+import { observer } from "mobx-react";
+import { clone, getSnapshot, applySnapshot } from "mobx-state-tree"
+
+class WishListItemView extends Component {
+    constructor() {
+        super()
+        this.state = {
+            isEditing: false
+        }
+    }
+    renderEditable() {
+        return (
+            <li>
+                <WishListItemEdit item={this.state.clone} />
+                <button onClick={this.onSaveEdit}>Save</button>
+                <button onClick={this.onCancelEdit}>Cancel</button>
+            </li>
+        )
+    }
+    onToggleEdit = () => {
+        this.setState({
+            isEditing: true,
+            clone: clone(this.props.item)
+        })
+    }
+    onCancelEdit = () => {
+        this.setState({
+            isEditing: false
+        })
+    }
+    onSaveEdit = () => {
+        applySnapshot(this.props.item, getSnapshot(this.state.clone))
+        this.setState({
+            isEditing: false,
+            clone: null
+        })
+    }
+    render() {
+        const { item } = this.props
+        return this.state.isEditing ? (
+                this.renderEditable()
+            ) : (
+            <li className="item">
+                { item.image && <img src = {item.image} alt=""/>}
+                <h3>{item.name}</h3>
+                <span>{item.price}</span>
+                <span>
+                    <button onClick={this.onToggleEdit}>Edit</button>
+                </span>
+            </li>
+        )
+    }
+}
+
+export default observer(WishListItemView)
--- src/components/WishListView.js
+++ src/components/WishListView.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { observer } from "mobx-react";
-import { WishListItemView } from "./WishListItemView";
-
+import WishListItemView from "./WishListItemView";
 const WishListView = ({ wishList}) => (
     <div className="list">
         <ul>

e601049dfa9c502c7cadc3d6abcb22e6fbd29aa5
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2019-03-06 11:20:19 +0530

    Added Compnents

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './assets/index.css';
+import App from './components/App';
+import { WishList } from "./models/WishList"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const wishList = WishList.create({
+    items: [
+        {
+            name: "item 1",
+            price: 10,
+            image: "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg"
+          },
+          {
+            name: "item 2",
+            price: 23.4,
+            image: "https://cdn130.picsart.com/289472792045201.jpg"
+          }
+    ]
+})
+ReactDOM.render(<App wishList={wishList}/>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+setInterval(() => {
+    wishList.items[0].changePrice(wishList.items[0].price +1)
+}, 1000)
